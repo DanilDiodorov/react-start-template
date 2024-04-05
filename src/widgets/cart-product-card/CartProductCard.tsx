0fb847f6f5d02348942c3f2b1f1698a85cae197d
danildiodorov <ddldiodorov@gmail.com>
2024-04-05 11:59:43 +0900

    test storybook

---
+++ src/widgets/cart-product-card/CartProductCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './CartProductCard.module.css';
+
+const CartProductCard = () => {
+  return <div></div>;
+};
+
+export default CartProductCard;
---
+++ src/widgets/product-full/ProductFullCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './ProductFullCard.module.css';
+import AddToCartButton from '../../features/addToCart/ui/AddToCartButton';
+
+export interface IProductFullCard {
+  title: string;
+  desc: string;
+  price: number;
+  image_url: string;
+}
+
+const ProductFullCard = ({ title, desc, price, image_url }: IProductFullCard) => {
+  return (
+    <div className={styles.main}>
+      <img className={styles.image} src={image_url} alt={title} />
+      <div>
+        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.price}>{price} ₽</div>
+        <div className={styles.desc}>{desc}</div>
+        <div className={styles.button}>
+          <AddToCartButton count={0} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductFullCard;
